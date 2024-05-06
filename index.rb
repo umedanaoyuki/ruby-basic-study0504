0ad302773b6853fa083086f3a77603230893fb49
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-05-06 23:17:13 +0900

    for文＋printの仕方

--- index.rb
+++ index.rb
@@ -1,22 +1,9 @@
-def fizz_buzz(n)
-  if n % 15 == 0
-    'Fizz Buzz'
-  elsif n % 3 == 0
-    'Fizz'
-  elsif n % 5 == 0
-    'Buzz'
-  else
-    n.to_s
-  end
-end
+numbers = [1, 2, 3, 4]
 
+sum = 0;
+numbers.each do |n|
 
-require 'minitest/autorun'
-
-class FizzBuzzTest < Minitest::Test
-  def test_fizz_buzz
-    assert_equal '0', fizz_buzz(1)
-    assert_equal '2', fizz_buzz(2)
-    assert_equal 'Fizz', fizz_buzz(3)
-  end
+  p n
+  sum += 1;
+  p %!合計は#{sum}です!
 end
